a369d8ac39150e4da1a651b012c5b5ba98384f71
Bruno <brunoppass@gmail.com>
2020-10-11 02:26:00 -0300

    master

--- src/tests/information.test.js
+++ src/tests/information.test.js
@@ -1,6 +1,6 @@
 const status = require('../../index')
-const Info = status.informationalResponse()
+const Info = status.information()
 
 test('', ()=>{
-
+    expect(JSON.stringify(Info.Continue())).toBe(`{}`)
 })
